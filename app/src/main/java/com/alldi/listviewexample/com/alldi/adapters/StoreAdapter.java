b66a973c30e1b0ac915da97c9074f16a49760420
alldifferent <alldifferent@naver.com>
2019-04-20 12:30:26 +0900

    StoreAdaper 클래스/ 생성자/ 멤버변수 작성

---
+++ app/src/main/java/com/alldi/listviewexample/com/alldi/adapters/StoreAdapter.java
@@ -0,0 +1,29 @@
+package com.alldi.listviewexample.com.alldi.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+
+import com.alldi.listviewexample.R;
+import com.alldi.listviewexample.com.alldi.datas.Store;
+
+import java.util.List;
+
+public class StoreAdapter extends ArrayAdapter<Store> {
+
+    Context mContext;
+    List<Store> mList;
+    LayoutInflater inf;
+
+    public StoreAdapter(Context context, List<Store> list){
+        super(context, R.layout.store_list_item, list);
+
+        mContext = context;
+        mList = list;
+        inf = LayoutInflater.from(mContext);
+
+    }
+
+
+
+}
